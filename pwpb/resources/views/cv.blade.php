458487178668e7089a358b390d2c48c7114efbe7
fazaainmlh <ina71106@gmail.com>
2023-08-23 11:39:11 +0700

    initial commit

---
+++ pwpb/resources/views/cv.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/cv.css">
+    <title>CV</title>
+</head>
+<body>
+    <main>
+        <nav class="nav">
+            <p>SF</p>
+            <ul class="nav-links">
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Interest">Interest</a></li>
+                <li><a href="#Skill">Skill</a></li>
+                <li><a href="#Education">Education</a></li>
+            </ul>
+        </nav>
+        <div class="page">
+            <div class="page-Home subpage" id="Home">
+                <p class="page-title">We Build This Page</p>
+                <p class="page-subtitle">Here you see, Ina's CV</p>
+            </div>
+
+            <div class="page-About subpage" id="About">
+                <p class="page-subtitle">Profile</p>
+                <img class="image" src="/img/ina.jpg" alt="">
+                <h3>Has named, Siti Fauziah In'Amillah and called as Ina. Born 16 years ago, and still dizzy for learning programmer materials.</h3>
+            </div>
+            <div class="page-Experience subpage" id="Interest">
+                <p class="page-subtitle">What am i interest to?</p>
+                <br>
+                <h3>love to hearing music but have interest to Psikologi things</h3>
+            </div>
+            <div class="page-Skill subpage" id="Skill">
+                <p class="page-subtitle">What i learn this far?</p>
+                <br> 
+                <h3>Still in the process of understanding HTML & CSS</h3>
+            </div>
+            <div class="page-Education subpage" id="Education">
+                <p class="page-subtitle">Education</p>
+                <br>
+                <p>SMKN 2 BANDUNG</p>
+                <p>SMPN 7 BANDUNG
+                <p>SDN 054 TIKUKUR</p>
+                </p>
+            </div>
+        </div>
+        <footer></footer>
+    </main>
+</body>
+</html>
