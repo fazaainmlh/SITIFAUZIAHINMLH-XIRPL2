af5df98ba634ae061b0458b6204d7c2c4f5d2883
fazaainmlh <ina71106@gmail.com>
2023-09-13 11:23:06 +0700

    company

---
+++ pwpb/resources/views/profile.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/profile.css">
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+            <img src="img/Logo-jurusan.png" width="70px" height="70px">
+            <h1>PPLG</h1>
+        </div>
+        <div class="nav-content">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="{{ url('cv') }}">CV</a></li>
+                <li><a href="#content">Contact</a></li>
+            </ul>
+            <!-- <button class="btn-pnk"><p>log in</p></button> -->
+        </div>
+    </div>
+    <div class="content">
+        <div class="content-header">
+            <h1>PENGEMBANGAN PERANGKAT LUNAK DAN GIM</h1>
+        </div>
+        <div class="content-intro">
+            <h3>Merupakan jurusan yang mempelajari dan mendalami cara-cara pengembangan perangkat lunak</h3>
+            <p>Seperti pembuatan Website, Aplikasi, game, serta semua yang berkaitan dengan pemograman </p>
+        </div>
+        <div class="holder-card">
+            <div class="card-1 card">
+                <p>Lorem</p>
+
+            </div>
+            <div class="card-2 card">
+                <p>Lorem</p>
+
+            </div>
+            <div class="card-3 card">
+                <p>Lorem</p>
+
+            </div>
+        </div>
+        <div class="content-footer" id="content">
+            <p>by: @fauziahinmlh</p>
+        </div>
+    </div>
+</body>
+</html>
